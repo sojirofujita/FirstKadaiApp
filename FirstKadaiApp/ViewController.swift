d46b1328e56a8f259a1adf289e2d2feb96eeea4c
USER <user@user-no-macbook-pro.local>
2019-03-17 11:56:34 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by USER on 2019/03/17.
+//  Copyright © 2019 Sf create. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
